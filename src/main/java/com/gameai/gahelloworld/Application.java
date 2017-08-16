0b77bbf4ae41e73279ade15411569eb909dd9aea
jonathanlee <jlee@serenetics.com>
2017-08-15 21:57:25 -0500

    Add health endpoint

--- src/main/java/com/gameai/gahelloworld/Application.java
+++ src/main/java/com/gameai/gahelloworld/Application.java
@@ -18,6 +18,16 @@ public class Application {
         return "Hello World!";
     }
 
+    /**
+     * <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
+     * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
+     */
+    @RequestMapping("/_ah/health")
+    public String healthy() {
+        // Message body required though ignored
+        return "Still surviving.";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
